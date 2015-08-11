b7a2c433cb995631ec9bb81113d5ce2aca6d764d
Daniel Peukert <dan.peukert@gmail.com>
2015-08-11 18:35:58 +0200

    ADD: An option to show a Chat link in the header

--- js/options.js
+++ js/options.js
@@ -123,6 +123,7 @@ function save_options() {
 	hidespamcomments = $("#hidespamcomments").prop('checked');
 	spamcommentregex = $("#spamcommentregex").val().trim();
 	wlbuttoncommunityapp = $("#wlbuttoncommunityapp").prop('checked');
+	showchatlink = $("#showchatlink").prop('checked');
 	show1clickgoo = $("#show1clickgoo").prop('checked');
 
 	// Profile Link Options
@@ -220,6 +221,7 @@ function save_options() {
 		'hidespamcomments': hidespamcomments,
 		'spamcommentregex': spamcommentregex,
 		'wlbuttoncommunityapp': wlbuttoncommunityapp,
+		'showchatlink': showchatlink,
 		'show1clickgoo': show1clickgoo,
 
 		'profile_steamgifts': profile_steamgifts,
@@ -425,6 +427,7 @@ function load_options() {
 		if (settings.hidespamcomments === undefined) { settings.hidespamcomments = false; chrome.storage.sync.set({'hidespamcomments': settings.hidespamcomments}); }
 		if (settings.spamcommentregex === undefined) { settings.spamcommentregex = "[\\u2500-\\u25FF]"; chrome.storage.sync.set({'spamcommentregex': settings.spamcommentregex}); }
 		if (settings.wlbuttoncommunityapp === undefined) { settings.wlbuttoncommunityapp = true; chrome.storage.sync.set({'wlbuttoncommunityapp': settings.wlbuttoncommunityapp}); }
+		if (settings.showchatlink === undefined) { settings.showchatlink = false; chrome.storage.sync.set({'showchatlink': settings.showchatlink}); }
 		if (settings.show1clickgoo === undefined) { settings.show1clickgoo = true; chrome.storage.sync.set({'show1clickgoo': settings.show1clickgoo}); }
 		if (settings.show_profile_link_images === undefined) { settings.show_profile_link_images = "gray"; chrome.storage.sync.set({'show_profile_link_images': settings.show_profile_link_images}); }
 		if (settings.profile_steamgifts === undefined) { settings.profile_steamgifts = true; chrome.storage.sync.set({'profile_steamgifts': settings.profile_steamgifts}); }
@@ -523,6 +526,7 @@ function load_options() {
 		$("#hidespamcomments").prop('checked', settings.hidespamcomments);
 		$("#spamcommentregex").val(settings.spamcommentregex);
 		$("#wlbuttoncommunityapp").prop('checked', settings.wlbuttoncommunityapp);
+		$("#showchatlink").prop('checked', settings.showchatlink);
 		$("#show1clickgoo").prop('checked', settings.show1clickgoo);
 		$("#profile_link_images_dropdown").val(settings.show_profile_link_images);
 
@@ -691,6 +695,7 @@ function load_translation() {
 			$("#show_spamcommentregex").text(localized_strings.options.customizespamcommentregex);
 			$("#steamcardexchange_text").text(localized_strings.options.steamcardexchange);
 			$("#wlbuttoncommunityapp_text").text(localized_strings.options.wlbuttoncommunityapp);
+			$("#showchatlink_text").text(localized_strings[settings.language].options.showchatlink);
 			$("#show1clickgoo_text").text(localized_strings.options.show1clickgoo);
 
 			$("#highlight_owned_default").text(localized_strings.theworddefault);
